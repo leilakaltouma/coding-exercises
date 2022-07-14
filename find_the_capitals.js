a8aece508bfc53edc597906471fc120296de38de
Leila <yasminekaltouma@gmail.com>
2022-07-14 21:22:49 +0000

    Find the capitals

---
+++ find_the_capitals.js
@@ -0,0 +1,14 @@
+//Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var capitals = function(word){
+    let arr = []
+    word = [...word]
+    
+    word.forEach((x,i) =>{
+      if(x === x.toUpperCase()){
+        arr.push(i)
+      }
+    })
+    
+    return arr
+  }
