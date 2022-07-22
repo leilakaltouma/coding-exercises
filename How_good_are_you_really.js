02425b9091c25d327c7a449b6c124b2443b7adb9
Leila <yasminekaltouma@gmail.com>
2022-07-22 22:52:09 +0000

    How good are you really

---
+++ How_good_are_you_really.js
@@ -0,0 +1,19 @@
+/*There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!*/
+
+function betterThanAverage(classPoints, yourPoints) {
+    let total= yourPoints
+    for(let i = 0; i < classPoints.length; i ++){
+      total += classPoints[i]
+    }
+    let average = total /(classPoints.length +1)
+    return yourPoints > average
+    
+      
+  }
+  
+  
