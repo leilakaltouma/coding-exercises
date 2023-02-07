d9d23d13befb2ee042b2f2475406010fbd31da2d
Leila <yasminekaltouma@gmail.com>
2023-02-07 23:13:44 +0000

    Javascript Mathematician

---
+++ Javascript_Mathematician.js
@@ -0,0 +1,16 @@
+/*You are writing a function that takes two sets of arguments of arbitrary length. The return value will be the sum of the values of all of the arguments.
+
+The function should contain at least 1 argument per set.
+
+calculate(1)(1) // should return 2
+calculate(1,1)(1) // should return 3
+calculate(1,1)(1,-1) // should return 2
+calculate(2,4)(3,7,1) // should return 17
+*/
+
+
+function calculate(...first) {
+    return function(...second){
+      return first.reduce((a,b) => a+b,0) + second.reduce((a,b) => a+b,0)
+    }
+  }
