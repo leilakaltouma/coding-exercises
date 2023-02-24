f04bad2738add16b8bca3e1d149b2b137d92ecb8
Leila <yasminekaltouma@gmail.com>
2023-02-24 22:47:05 +0000

    Changing Letters

---
+++ Changing_letters.js
@@ -0,0 +1,17 @@
+/*When provided with a String, capitalize all vowels
+
+For example:
+
+Input : "Hello World!"
+
+Output : "HEllO WOrld!"
+
+Note: Y is not a vowel in this kata.
+*/
+
+
+function swap (string) {
+  let vowels = 'aeiou'
+  return string.split('').map(x => vowels.includes(x) ? x.toUpperCase() : x).join('')
+}
+
