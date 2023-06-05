829661974948ab375b84be22f11edde3c082e322
Leila <yasminekaltouma@gmail.com>
2023-06-05 22:42:57 +0000

    Make Acronym

---
+++ Make_Acronym.js
@@ -0,0 +1,27 @@
+/*Write function which takes a string and make an acronym of it.
+
+Rule of making acronym in this kata:
+
+split string to words by space char
+take every first letter from word in given string
+uppercase it
+join them toghether
+Eg:
+
+Code wars -> C, w -> C W -> CW
+Note: There will be at least two words in the given string!
+*/
+
+
+
+function toAcronym(inp){
+  let str = ""
+  inp = inp.split(' ')
+  for(let i = 0; i< inp.length; i++){
+    str += inp[i][0].toUpperCase()
+  }
+  return str
+}
+
+
+
