61450191105174ec194ea0d3f98593bb8f76a466
Leila <yasminekaltouma@gmail.com>
2023-07-29 15:22:45 +0000

    tiny change

--- vowel.js
+++ vowel.js
@@ -1,4 +1,4 @@
-/*Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.*/
+/*Implement the function which returns true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.*/
 
 String.prototype.vowel = function() {
   return /^[aeiou]$/i.test(this)
