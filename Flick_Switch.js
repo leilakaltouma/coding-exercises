bb1e96177df0cb90afe97b36b72aec1b07c0e306
Leila <yasminekaltouma@gmail.com>
2023-12-17 22:06:42 +0000

    Flick switch

---
+++ Flick_Switch.js
@@ -0,0 +1,16 @@
+/*Task
+Create a function that always returns true for every item in a given list. However, if an element is the word "flick", switch to always returning the opposite boolean value.
+
+Examples
+['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+
+['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+
+['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False
+*/
+
+
+function flickSwitch(arr){
+    let flicky = true
+    return arr.map(x => x == 'flick'? flicky =!flicky : flicky)
+    }
